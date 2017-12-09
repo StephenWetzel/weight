80ba467352280d406fa084703e14a13e2cb9338a
Stephen Wetzel <stephen.wetzel1@gmail.com>
2017-12-09 17:36:17 -0500

    HTML graph

---
+++ chart.js
@@ -0,0 +1,49 @@
+  var chart = c3.generate({
+    size: {
+    },
+    data: {
+      url: 'weight_two.tsv',
+      mimeType: 'tsv',
+      x: 'date_time',
+      xFormat: '%Y-%m-%d %H:%M:%S', // 'xFormat' can be used as custom format of 'x'
+      axes: {
+        weight: 'y',
+        avg_weight: 'y',
+        bmi: 'y2',
+      },
+      types: {
+        weight: 'scatter',
+        avg_weight: 'spline',
+        bmi: 'scatter',
+        date_time: 'x'
+      }
+    },
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%b %-m %Y',
+          count: 25,
+        }
+      },
+      y: {
+        show: true,
+        label: {
+          text: "Weight (lbs)"
+        }
+      },
+      y2: {
+        show: true,
+        label: {
+          text: "BMI"
+        }
+      }
+    },
+    zoom: {
+      enabled: true,
+      rescale: true
+    }
+
+
+
+  });
