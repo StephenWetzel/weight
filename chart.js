5e6d341332666876ea995deec52529160816cbbc
Stephen Wetzel <stephen.wetzel1@gmail.com>
2017-12-09 17:46:36 -0500

    Add grid, hide bmi by default

--- chart.js
+++ chart.js
@@ -1,49 +1,53 @@
-  var chart = c3.generate({
-    size: {
+var chart = c3.generate({
+  size: {
+  },
+  data: {
+    url: 'weight_two.tsv',
+    mimeType: 'tsv',
+    x: 'date_time',
+    xFormat: '%Y-%m-%d %H:%M:%S', // 'xFormat' can be used as custom format of 'x'
+    axes: {
+      weight: 'y',
+      avg_weight: 'y',
+      bmi: 'y2',
     },
-    data: {
-      url: 'weight_two.tsv',
-      mimeType: 'tsv',
-      x: 'date_time',
-      xFormat: '%Y-%m-%d %H:%M:%S', // 'xFormat' can be used as custom format of 'x'
-      axes: {
-        weight: 'y',
-        avg_weight: 'y',
-        bmi: 'y2',
-      },
-      types: {
-        weight: 'scatter',
-        avg_weight: 'spline',
-        bmi: 'scatter',
-        date_time: 'x'
-      }
-    },
-    axis: {
-      x: {
-        type: 'timeseries',
-        tick: {
-          format: '%b %-m %Y',
-          count: 25,
-        }
-      },
-      y: {
-        show: true,
-        label: {
-          text: "Weight (lbs)"
-        }
-      },
-      y2: {
-        show: true,
-        label: {
-          text: "BMI"
-        }
-      }
-    },
-    zoom: {
-      enabled: true,
-      rescale: true
+    types: {
+      weight: 'scatter',
+      avg_weight: 'spline',
+      bmi: 'scatter',
+      date_time: 'x'
     }
+  },
+  axis: {
+    x: {
+      type: 'timeseries',
+      tick: {
+        format: '%b %-m %Y',
+        count: 25,
+      }
+    },
+    y: {
+      show: true,
+      label: {
+        text: "Weight (lbs)"
+      }
+    },
+    y2: {
+      show: true,
+      label: {
+        text: "BMI"
+      }
+    }
+  },
+  zoom: {
+    enabled: true,
+    rescale: true
+  },
+  grid: {
+    y: {
+      show: true
+    }
+  }
+});
 
-
-
-  });
+chart.hide('bmi');
